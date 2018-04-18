2258f30322833d08dd27b51d3a784a325b640d97
Ozair Khan <ozairsaddress@gmail.com>
2018-04-18 14:05:06 -0600

    finishing functionality

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
+
 
 exports.twitter = {
-  consumer_key: "DBeH70y5Fr2fm2fCQyZXLAzTk",
-  consumer_secret: "OQQvQYt1gwB3tpnJXWlDDNwMFC5Q2OP16Ss2pt8hIjxkP7F3SC",
-  access_token_key: "981226961816248321-B9PmmR7n2beNFPLi4RUsFi82SazXniX",
-  access_token_secret: "Qmd1vSXr50QWG6VQOMhi6hNN1TCQCnuhJF9UE8VW2V2xV"
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: "0472f90d85f94d19808e8a29702f3e52",
-  secret: "56c75058a58642de8e3c70f143d9d390"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
